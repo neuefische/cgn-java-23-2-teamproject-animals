079c0d69c937900bc984b70e2294585d6fd015fd
StormTrooper-007 <chigbo.c.o@gmail.com>
2023-06-29 12:07:40 +0200

    added AnimalServiceIntegrationTest

---
+++ backend/src/test/java/de/neuefische/backend/AnimalServiceIntegrationTest.java
@@ -0,0 +1,38 @@
+package de.neuefische.backend;
+
+import de.neuefische.backend.models.Animal;
+import de.neuefische.backend.repositories.AnimalRepository;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AnimalServiceIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    void get_list_of_products() throws Exception {
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/animals"));
+
+        resultActions.andExpect(status().isOk()).andReturn();
+        resultActions.andExpect(jsonPath("$", hasSize(0)));
+    }
+}
