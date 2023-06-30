bbe28ec6c720e0c7cbbf36e241caf6f47a419439
sharoz <sharoz.ahmad@gmx.de>
2023-06-30 12:05:36 +0200

    add animal frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,31 @@ import axios from "axios";
 
 function App() {
     const [animals, setAnimals] = useState([]);
-
+    const [name, setName] = useState("")
     useEffect(() => {
         axios.get('/api/animals')
             .then((response) => setAnimals(response.data))
             .catch((error) => console.error(error));
     }, []);
 
+    function addAnimal() {
+        axios.post("/api/animal", {
+            id: animals.length + 1,
+            name
+        }).then((response) => console.log(response.data)).catch(error => console.log(error))
+    }
+
     return (
-    <><div>
-        {animals.map((animal: Animal) => (<div key={animal.id}>{animal.name}</div>))}
-    </div>
-    </>
+        <>
+            <div>
+                <form onSubmit={addAnimal}>
+                    <input type="text" value={name} onChange={(event) =>
+                        setName(event.target.value)}/>
+                    <button>add animal</button>
+                </form>
+                {animals.map((animal: Animal) => (<div key={animal.id}>{animal.name}</div>))}
+            </div>
+        </>
     )
 }
 
