dd68f4859c9115bac020dc55ff5fe5d355da1909
sharoz <sharoz.ahmad@gmx.de>
2023-06-30 11:37:38 +0200

    add integration test

--- backend/src/test/java/de/neuefische/backend/AnimalServiceIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/AnimalServiceIntegrationTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,5 +31,19 @@ class AnimalServiceIntegrationTest {
 
         resultActions.andExpect(status().isOk()).andReturn();
         resultActions.andExpect(jsonPath("$", hasSize(3)));
+    }
+
+    @Test
+    @DirtiesContext
+    void confirms_a_animal_was_added() throws Exception {
+        String requestBody = """
+                {"id":"1", "name":"dog"}
+                """;
+        ResultActions resultActions = mockMvc.perform(post("/api/animal")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody));
+
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$", hasSize(1)));
     }
 }
