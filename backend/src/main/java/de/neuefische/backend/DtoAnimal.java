6e168f2ba0d09a71d7d0a3f44fefbaa2656f744b
sharoz <sharoz.ahmad@gmx.de>
2023-07-07 16:23:45 +0200

    delete @Document Annotation

--- backend/src/main/java/de/neuefische/backend/DtoAnimal.java
+++ backend/src/main/java/de/neuefische/backend/DtoAnimal.java
@@ -3,12 +3,10 @@ package de.neuefische.backend;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document("animals")
 public class DtoAnimal {
     private String name;
 }
