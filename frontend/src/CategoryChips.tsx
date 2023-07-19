fd71b5191d268847c0b936e67a1b57206074e492
Alexander Guenzel <alexander.guenzel@informatik.hs-fulda.de>
2023-07-19 15:42:05 +0200

    animal categrory chips color

--- frontend/src/CategoryChips.tsx
+++ frontend/src/CategoryChips.tsx
@@ -1,14 +1,18 @@
 import {Chip, Grid, Stack} from "@mui/material";
 import {Item} from "./Utils.tsx";
 
+
 function CategoryChips() {
     return (
         <Grid item xs={12} sx={{mt: 2}}>
             <Item variant="outlined">
-                <Stack direction="row" spacing={1}
-                       sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <Chip label="Dogs" variant="outlined"/>
-                    <Chip label="Cats" variant="outlined"/>
+                <Stack direction="row" spacing={4}
+                       sx={{display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center"}}>
+                    <Chip label="Dogs" variant="outlined" sx={{bgcolor: "turquoise"}}/>
+                    <Chip label="Cats" variant="outlined" sx={{bgcolor: "blueviolet"}}/>
+                    <Chip label="Bird" variant="outlined" sx={{bgcolor: "orange"}}/>
+                    <Chip label="Reptile" variant="outlined" sx={{bgcolor: "yellow"}}/>
+                    <Chip label="Fish" variant="outlined" sx={{bgcolor: "lightBlue"}}/>
                 </Stack>
             </Item>
         </Grid>
