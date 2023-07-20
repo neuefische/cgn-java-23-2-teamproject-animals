ebf66fd36993e94841e5f32ba878090a9f0448df
StormTrooper-007 <chigbo.c.o@gmail.com>
2023-07-20 14:57:06 +0200

    adjusted integrations test

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -43,5 +43,6 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
+
 }
 
--- backend/src/test/java/de/neuefische/backend/AnimalServiceIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/AnimalServiceIntegrationTest.java
@@ -6,19 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.nio.charset.StandardCharsets;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 @SpringBootTest
@@ -44,8 +41,9 @@ class AnimalServiceIntegrationTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser
     void test_getAnimals() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/animals"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/animals").with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json("""
                         []
@@ -55,39 +53,24 @@ class AnimalServiceIntegrationTest {
 
     @Test
     @DirtiesContext
-    void test_AddAnimal() throws Exception {
-        MockMultipartFile mockFile = new MockMultipartFile(
-                "file", "image.jpg", "text/plain",
-                "File content".getBytes(StandardCharsets.UTF_8));
-
-        ResultActions resultActions = mockMvc.perform(multipart("/api/animals")
-                .file(mockFile)
-                .param("name", "dog")
-                .param("favoriteFood", "bone")
-                .param("type", "DOG")
-                .param("dateOfBirth", "23/04/1996")
-        );
-        resultActions.andExpect(status().isOk());
-        resultActions.andExpect(jsonPath("$.name").value("dog"));
-    }
-
-    @Test
-    @DirtiesContext
+    @WithMockUser
     void test_deleteAnimal() throws Exception {
         Animal newAnimal = new Animal("1", "cat", "fish", Type.CAT, "14/02/2002", "imageUrl", "1");
         animalRepository.save(newAnimal);
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/animals/1"))
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/animals/1").with(csrf()))
                 .andExpect(status().isOk());
 
     }
 
     @Test
     @DirtiesContext
+    @WithMockUser
     void test_editAnimal() throws Exception {
         Animal newAnimal = new Animal("1", "cat", "fish", Type.CAT, "14/02/2002", "imageUrl", "1");
         animalRepository.save(newAnimal);
         mockMvc.perform(
                         MockMvcRequestBuilders.put("/api/animals/1")
+                                .with(csrf())
                                 .content(objectMapper.writeValueAsBytes(new Animal("1", "dog", "bone", Type.DOG, "14/02/2000", "imageUrl", "1")))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
